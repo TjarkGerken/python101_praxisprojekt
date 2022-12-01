6748546bec8493906d0fe47f227b38b32f2f45c5
TjarkGerken <96004366+TjarkGerken@users.noreply.github.com>
2022-12-01 21:33:19 +0100

    Fixed that the engines get wrongly counted.

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ fp = "./data/vehicle_data.xlsx"
 etl = ETL(fp=fp)
 etl.run()
 
-# query1 = Analyse(dataframe=etl.df_return, start_date="01.01.2014", end_date="31.12.2020", file_save_path="./plots/",
-#          query=1)
+query1 = Analyse(dataframe=etl.df_return, start_date="01.01.2014", end_date="31.12.2020", file_save_path="./plots/",
+                 query=1)
 query2 = Analyse(dataframe=etl.df_return, start_date="01.01.2017", end_date="01.01.2021", file_save_path="./plots/",
                  query=2)
-# query3 = Analyse(dataframe=etl.df_return, file_save_path="./plots/", query=3)
+query3 = Analyse(dataframe=etl.df_return, file_save_path="./plots/", query=3)
 
-# print("First Query:")
-# query1.dashboard()
-# print("Second Query:")
-print(query2.filter_by_engines(["OM 934", "OM 936", "OM 470", "OM 471"]))
-# print("Third Query:")
-# query3.dashboard()
+print("First Query:")
+query1.dashboard()
+print("Second Query:")
+query2.dashboard()
+print("Third Query:")
+query3.dashboard()
